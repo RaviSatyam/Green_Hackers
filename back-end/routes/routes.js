560561185194ef9fd30d01578825a0f0becfebea
RaviSatyam <rsatyam1296@gmail.com>
2023-06-11 01:11:09 +0530

    Created Government APIs

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -6,6 +6,13 @@ const {registerEmitter,getAllEmitters,getEmitterById,checkEmitterExistanceByAccI
 }=require("../controllers/emitter-controller.js");
 
 
+
+const {registerGovt,getGovtDetails,acceptEmitterRegistrationRequest,findUserRegisterDetails,rejectEmitterRegistrationRequest,
+    acceptEmitterCCAllowanceRequest,paybackRequestToEmitter,freezeAccountByGovt, getEmitterDetailsRequestForCCFromMRVById,getPaybackDetailsRequestByGovtById,
+    paybackRequestToEmitterByAcId,checkTokenBalance, getPaybackDetailsRequestByGovt, 
+    getEmitterDetailsRequestForCCFromMRV}=require("../controllers/govt-controller.js");
+    
+
 //Routes for Emitter
 router.route("/addEmitter").post(registerEmitter);
 router.route("/getAllEmitter").get(getAllEmitters);
@@ -19,6 +26,22 @@ router.route("/deleteAllEmitters").delete(deleteAllEmitters);
 router.route("/getAllTickets").get(getAllTicketList);
 router.route("/getTktById").get(getTicketById);
 router.route("/deleteAllTicket").delete(deleteAllTicket);
+
+// routes for government
+router.route("/addGovt").post(registerGovt);
+router.route("/findUserRegisterDetails").get(findUserRegisterDetails);
+router.route("/getGovtDetails").get(getGovtDetails);
+router.route("/acceptEmitterRegReq").put(acceptEmitterRegistrationRequest);
+router.route("/rejectEmitterRegReq").delete(rejectEmitterRegistrationRequest);
+router.route("/acceptCCallowance").put(acceptEmitterCCAllowanceRequest);
+router.route("/paybackReqToAllEmitter").put(paybackRequestToEmitter);
+router.route("/paybackReqToEmitterByAcId").put(paybackRequestToEmitterByAcId);
+router.route("/freezeAccountByGovt").get(freezeAccountByGovt);
+router.route("/tokenBal").get(checkTokenBalance);
+router.route("/getPaybackDetailsRequestByGovt").get(getPaybackDetailsRequestByGovt);
+router.route("/getEmitterDetailsRequestForCCFromMRV").get(getEmitterDetailsRequestForCCFromMRV);
+router.route("/getEmitterDetailsRequestForCCFromMRVById").get(getEmitterDetailsRequestForCCFromMRVById);
+router.route("/getPaybackDetailsRequestByGovtById").get(getPaybackDetailsRequestByGovtById);
 
 
 module.exports=router;
--- back-end/utils/utils.js
+++ back-end/utils/utils.js
@@ -8,3 +8,28 @@ module.exports.randomTktGenerator = () => {
     //console.log(ticketId)
     return ticketId;
 }
+
+
+
+
+module.exports.mapPrivateKeywithId = (accountID) => {
+
+    const Acc1 = process.env.Account1_Id;
+    const Acc2 = process.env.Account2_Id;
+    const Acc3 = process.env.Account3_Id;
+    const pvt1 = process.env.Account1_PVKEY;
+    const pvt2 = process.env.Account2_PVKEY;
+    const pvt3 = process.env.Account3_PVKEY;
+    const valueTable = {
+
+        [Acc1]: pvt1,
+
+        [Acc2]: pvt2,
+
+        [Acc3]: pvt3
+
+    };
+    // const x = valueTable[region][industryType] || 0;
+    return valueTable[accountID];
+}
+
